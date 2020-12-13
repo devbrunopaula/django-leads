6654e620106708b19fd7250729de335349888cd1
Bruno Paula <devbrunopaula@gmail.com>
2020-12-12 19:21:51 -0500

    k

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')wpo2_bf1uyx8*7@k*l&09(r5(a*-@_1kctuvkmvz*uzf7840+'
+SECRET_KEY = secrets.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
